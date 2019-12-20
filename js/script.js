1428d34ce91747c26c015438ceb9567eaee4e4ad
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-20 15:34:11 +0300

    /

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-var array = [1325412, 34, 64, 12, -43, -1248, 1293];
+var array = [1325412, 34, 64, 12, -43, -1248, 1293];//Числа массива
 var max = getMaxValue(array);
 var min = getMinValue(array);
-console.log(max);
-console.log(min);
+console.log(max); //Вывод максимального числа вконсоль
+console.log(min); //Вывод максимального числа вконсоль
  
 //высчитываем максимальный элемента массива
 function getMaxValue(array){
