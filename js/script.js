c1e291148e3353335134cad17039ce5f9ceb8bd1
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-20 15:25:27 +0300

    array

--- js/script.js
+++ js/script.js
@@ -3,3 +3,10 @@ var max = getMaxValue(array);
 var min = getMinValue(array);
 console.log(max);
 console.log(min);
+
+function getMaxValue(array) {
+    var max = array[0];
+    for (var i = 0; i < array.lenght; i++) {
+        if (max < array[i]) max = array[i]; 
+    }
+}
